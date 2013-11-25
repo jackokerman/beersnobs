fa9053db53f6b2fcab9f2c343ea6964833c4cf7b
Jack Okerman <Jack@Jacks-MacBook-Pro-2.local>
2013-11-25 13:42:13 -0800

    Added utilites.php and beer.jpg

---
+++ include/utilities.php
@@ -0,0 +1,23 @@
+<?php
+    function typeDisplayToDb($display_type) {
+        $words = explode(" ", $display_type);
+        $db_type = "";
+        for ($i = 0; $i < count($words); $i++) {
+            $db_type = $db_type . lcfirst($words[$i]);
+            if ($i < count($words) - 1)
+                $db_type = $db_type . "-";
+        }
+        return $db_type;
+    }
+
+    function typeDbToDisplay($display_type) {
+        $words = explode("-", $display_type);
+        $db_type = "";
+        for ($i = 0; $i < count($words); $i++) {
+            $db_type = $db_type . ucfirst($words[$i]);
+            if ($i < count($words) - 1)
+                $db_type = $db_type . " ";
+        }
+        return $db_type;
+    }
+?>
