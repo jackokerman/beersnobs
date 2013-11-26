efbcf530d3e2d2ab88c88fbb156145301193dbe1
willjleong <willjleong@gmail.com>
2013-11-25 20:15:24 -0800

    added top beer printout

--- include/topbeers.php
+++ include/topbeers.php
@@ -1,26 +1,42 @@
 <h1>TopBeers</h1>
 
-<table class = "table-bordered">
-	<tr>
-		<th>Beer Name</th>
-		<th>Type</th>
-		<th>Description</th>
-	</tr>	
+
 <?php
     include("include/dbconnect.php");
+    include("include/utilities.php");
     // define variables and set to empty values
 
-    $dbbeerrecord = mysqli_query($dblocalhost,"SELECT * FROM beer")
-        or die("Problem writing to table: " . mysqli_error($dblocalhost));
-    while($row = mysqli_fetch_array($dbbeerrecord))
-  	{
-  		echo "<tr>";
-  		echo "<td>" .$row['beer_name'] . "</td>";
-  		echo "<td>" . $row['type'] . "</td>";
-  		echo "<td>" . $row['description'] . "</td>";
-  		echo "</tr>";
-  	}
-  	mysqli_close($dblocalhost);
-?>
+    // $dbbeerrecord = mysqli_query($dblocalhost,"SELECT * FROM beer")
+    //     or die("Problem writing to table: " . mysqli_error($dblocalhost));
+    // while($row = mysqli_fetch_array($dbbeerrecord))
 
-</table>
+    $beerRank = array();
+    $result = mysqli_query($dblocalhost, "SELECT beer_name FROM beer")
+            or die("Problem querrying table: " . mysqli_error($dblocalhost));
+    while ($row = mysqli_fetch_row($result)){
+        $beerRank[$row[0]]=getReviewsAvg($row[0],$dblocalhost);
+    }
+    arsort($beerRank);
+    foreach ($beerRank as $key => $value) {
+        echo "$key = $value" . "<br>";
+    }
+
+
+    function getReviewsAvg($name, $db) {
+        $tasteAvg = getColAverage("taste", $name, $db);
+        $aromaAvg = getColAverage("aroma", $name, $db);
+        $valueAvg = getColAverage("value", $name, $db);
+        $overallAvg = ($tasteAvg + $aromaAvg + $valueAvg) / 3;
+        return $overallAvg;
+    }
+
+    function getColAverage($col, $name, $db) {
+        $result = mysqli_query($db, "SELECT AVG({$col}) FROM review WHERE beer_name='{$name}' ")
+            or die("Problem querrying table: " . mysqli_error($db));
+        $row = mysqli_fetch_row($result);
+        $avg = $row[0];
+        return $avg;
+    }
+
+    mysqli_close($dblocalhost);
+?>
